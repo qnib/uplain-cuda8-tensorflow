a00fde69349ca8e8bfb7a721b45f895896985a98
Christian Kniep <christian@qnib.org>
2018-10-22 16:01:59 +0200

    adjust start-script

--- Dockerfile
+++ Dockerfile
@@ -100,10 +100,7 @@ RUN mkdir -p /root/.jupyter/ \
  # IPython
  EXPOSE 8888
 
-COPY bin/start_jupyter.sh /
-COPY ./notebooks /notebooks/basic
-RUN git clone https://github.com/aymericdamien/TensorFlow-Examples.git /tmp/notebooks \
- && mv /tmp/notebooks/notebooks /notebooks/advanced
 RUN pip install opencv-python keras==2.1.3 tqdm
 #COPY opt/entry/nvidia-reset-gpu.sh /opt/entry/20-nvidia-reset-gpu.sh
-CMD ["/start_jupyter.sh","--notebook-dir", "/notebooks"]
+COPY bin/start_jupyter.sh /
+CMD ["/start_jupyter.sh"]
--- bin/start_jupyter.sh
+++ bin/start_jupyter.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-jupyter notebook  --allow-root --ip 0.0.0.0 --notebook-dir /notebooks $@
+: ${JUPYTER_ARGS:="${JUPYTER_ARGS}"}
+: ${JUPYTER_NOTEBOOK_DIR:="/notebooks"}
+: ${JUPYTER_BASE_URL:="/"}
+
+JUPYTER_ARGS="--allow-root --ip 0.0.0.0 --notebook-dir ${JUPYTER_NOTEBOOK_DIR} --NotebookApp.base_url=${JUPYTER_BASE_URL}"
+if [[ "X$JUPYTER_WEBSOCKET_URL" != "X" ]];then
+  JUPYTER_ARGS="${JUPYTER_ARGS} --NotebookApp.websocket_url=${JUPYTER_WEBSOCKET_URL}"
+fi
+if [[ "X$JUPYTER_API_TOKEN" != "X" ]];then
+  JUPYTER_ARGS="${JUPYTER_ARGS} --NotebookApp.token=${JUPYTER_API_TOKEN}"
+fi
+
+echo ">> jupyter notebook ${JUPYTER_ARGS}  $@"
+jupyter notebook ${JUPYTER_ARGS}  $@
